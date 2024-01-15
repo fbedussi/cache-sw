2af014a85427ad8051332f8d297fb05fdb344916
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-01-15 23:34:54 +0100

    fix: hard reload issue

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import './cat-fact.js'
 
-navigator.serviceWorker.register("/sw.js", {scope: "./"}).then(
-  (registration) => {
+const registerSw = async () => {
+  try {
+    const registration = await navigator.serviceWorker.register("/sw.js", {scope: "./"})
     registration.update();
+    console.log("Service worker registration succeeded:", registration)
+  } catch (error) {
+    console.error(`Service worker registration failed: ${error}`)
+  }
+}
 
-    console.log("Service worker registration succeeded:", registration);
-  },
-  (error) => {
-    console.error(`Service worker registration failed: ${error}`);
-  })
+registerSw()
+
+// reload if page is not controlled as a workaround to the fact that when the page is force reloaded 
+// the SW doesn't work
+// https://github.com/mswjs/msw/issues/98
+if (!navigator.serviceWorker.controller) {
+  location.reload()
+}
--- service.js
+++ service.js
@@ -1,54 +1,51 @@
 const tags = {}
 
- navigator.serviceWorker.ready.then((registration) => {
-  console.log('service worker ready')
- })
- 
 export const getCatFact = async (signal, tag, cleanups) => {
-  return navigator.serviceWorker.ready.then((registration) => {
-    const cb = (event) => {
-      if (event.data.type === 'CACHE_ENTRY_INVALIDATED') {
-        placeCall()
-      }
-    }
-
-    const cleanup = () => {
-      navigator.serviceWorker.removeEventListener('message', cb)
-    }
-    cleanup()
-    cleanups.push(cleanup)
-
-    navigator.serviceWorker.startMessages()
-    navigator.serviceWorker.addEventListener('message', cb)
-
-    const placeCall = () => {
-      const req = new Request('https://catfact.ninja/fact', {
-        method: 'GET'
-      })
-
-      tags[tag] = {
-        url: req.url,
-        method: req.method,
-        body: JSON.stringify(req.body),
-        ttl: 1000 * 60 * 60,
+  return navigator.serviceWorker.ready
+    .then((registration) => {
+      const cb = (event) => {
+        if (event.data.type === 'CACHE_ENTRY_INVALIDATED') {
+          placeCall()
+        }
       }
 
-      registration.active.postMessage({
-        type: 'TTL',
-        req: tags[tag],
-      });
+      const cleanup = () => {
+        navigator.serviceWorker.removeEventListener('message', cb)
+      }
+      cleanup()
+      cleanups.push(cleanup)
 
-      fetch(req)
-        .then(res => {
-          return res.json()
-        })
-        .then(res => {
-          signal.value = res.fact
-        })
-    }
+      navigator.serviceWorker.startMessages()
+      navigator.serviceWorker.addEventListener('message', cb)
 
-    placeCall()
-  });
+      const placeCall = () => {
+        const req = new Request('https://catfact.ninja/fact', {
+          method: 'GET'
+        })
+
+        tags[tag] = {
+          url: req.url,
+          method: req.method,
+          body: JSON.stringify(req.body),
+          ttl: 1000 * 60 * 60,
+        }
+
+        registration.active.postMessage({
+          type: 'TTL',
+          req: tags[tag],
+        });
+
+        fetch(req)
+          .then(res => {
+            return res.json()
+          })
+          .then(res => {
+            signal.value = res.fact
+          })
+      }
+
+      placeCall()
+    });
 }
 
 export const invalidateCacheEntry = (tag) => {
